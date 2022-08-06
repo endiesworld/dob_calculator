0e15f93f17ef551add9571313fbf4cdec5505783
Emmanuel <endiesworld@gmail.com>
2022-08-06 17:12:38 +0100

    Added throtling

--- aga_calculator/aga_calculator/settings.py
+++ aga_calculator/aga_calculator/settings.py
@@ -123,3 +123,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '3/minute',
+        'user': '1000/day'
+    }
+}
--- aga_calculator/calculate_age/views.py
+++ aga_calculator/calculate_age/views.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timedelta, date
 # from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, throttle_classes
+from rest_framework.throttling import AnonRateThrottle
 
 
 # Create your views here.
-@api_view()
+@api_view(['GET'])
+@throttle_classes([AnonRateThrottle])
 def calculate_age(request):
     year = int(request.GET['year'])
     month = int(request.GET['month'])
