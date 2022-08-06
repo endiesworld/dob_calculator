e1458ace677fdf963617b9c4550d59256e75d237
Emmanuel <endiesworld@gmail.com>
2022-08-06 12:45:02 +0100

    Added venv

--- dob_calculator.py
+++ dob_calculator.py
@@ -1,3 +1,11 @@
+from datetime import datetime, timedelta, date
 
-def calculate_age(dob):
-    pass
+
+def calculate_age(dob: list):
+    dob = date(dob[0], dob[1], dob[2])
+    age = datetime.now().date() - dob
+    age_years = age // timedelta(days=365)
+    print(age_years)
+
+
+calculate_age([1986, 12, 17])
